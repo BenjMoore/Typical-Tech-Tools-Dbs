41a55d60a6a12a23c7480e3cc9f3790e066ac8f6
Benjamin Moore <moorebenjamin30@gmail.com>
2024-11-13 13:44:51 +1000

    push

--- TypicalTechTools/obj/Debug/net6.0/TypicalTechTools.AssemblyInfo.cs
+++ TypicalTechTools/obj/Debug/net6.0/TypicalTechTools.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypicalTechTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6685e812b632a0f5351cd6611a9a03b2b5d70c4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56357657a20aaacbd697f88a515258b58d911b90")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypicalTechTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypicalTechTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
